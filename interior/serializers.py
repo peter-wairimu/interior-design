e1ead7cbf77a4fdf2283821920daadb08375f46e
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-11-03 08:45:26 +0300

    implemented the register class functionality

--- interior/serializers.py
+++ interior/serializers.py
@@ -6,3 +6,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields =['id', 'name', 'email', 'password']
+        extra_kwargs = {
+
+            'password': {'write_only': True}
+        }
+
+
+    def create(self,validated_data):
+        password = validated_data['password']
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+
+        return instance
--- interior/views.py
+++ interior/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
 
 class RegisterView(APIView):
     def post(self, request):
-        pass
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
     
 
